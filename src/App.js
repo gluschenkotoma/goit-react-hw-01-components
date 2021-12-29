e05db798270e2f72e02af42ef34da2aa36e94f39
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2021-12-29 13:51:40 +0200

    globalstyles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { GlobalStyle } from 'components/GlobalStyle';
 import { Profile } from 'components/Profile/Profile';
 import user from 'user.json';
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Profile user={user} />
     </>
   );
